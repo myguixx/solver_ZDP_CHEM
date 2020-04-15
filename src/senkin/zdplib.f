91444bb4f47bb34d1eedd0f445ca187b5a07f01f
syoukera <s.akira2986@gmail.com>
2020-04-15 17:33:30 +0900

    Convert unit from CHEMKIN to ZDPlasKin

--- src/senkin/zdplib.f
+++ src/senkin/zdplib.f
@@ -16,7 +16,8 @@ C
       IMPLICIT DOUBLE PRECISION (A-H, O-Z), INTEGER (I-N)
       LOGICAL gas_heating
       DIMENSION Z(*), ZP(*), DELTA(*), RPAR(*), IPAR(*),
-     1          source_terms_species(83)
+     1          source_terms_species(species_max), 
+     2          X(species_max), dens(species_max)
       COMMON /RES1/ P
 C
 C  Residual of differential equations for constant pressure case
@@ -30,58 +31,69 @@ C    RPAR   = array of reaction pre-exponential constants
 C
       KK     = IPAR(1)
       IPRCK  = IPAR(2)
-      IPRD   = IPAR(3)
-      IPWT   = IPAR(6)
-      IPWDOT = IPAR(7)
-      IPH    = IPAR(8)
+!       IPRD   = IPAR(3)
+!       IPWT   = IPAR(6)
+!       IPWDOT = IPAR(7)
+!       IPH    = IPAR(8)
       IPICK  = IPAR(9)
-      LOUT   = IPAR(10)
-      II     = IPAR(11)
-C
-C        MODIFY CHEMKIN WORK ARRAY FOR PRE-EXPONENTIAL
-C
-      DO 15 I = 1, II
-        CALL CKRDEX (-I, RPAR(IPRCK), RPAR(IPRD+I-1))
-15    CONTINUE
-C
-C         CALL CHEMKIN SUBROUTINES
-C
-      CALL CKRHOY (P, Z(1), Z(2), IPAR(IPICK), RPAR(IPRCK), RHO)
-      CALL CKCPBS (Z(1), Z(2), IPAR(IPICK), RPAR(IPRCK), CPB)
-      CALL CKWYP  (P, Z(1), Z(2), IPAR(IPICK), RPAR(IPRCK),
-     1             RPAR(IPWDOT))
-      CALL CKHMS  (Z(1), IPAR(IPICK), RPAR(IPRCK), RPAR(IPH))
-      IF (RHO .EQ. 0.0) THEN
-         WRITE (LOUT, '(/1X,A)') 'Stop, zero density in RCONP.'
-         STOP
-      ENDIF
-      VOLSP = 1. / RHO
-C
-C         ENERGY EQUATION
-C
-      SUM = 0.
-      DO 100 K = 1, KK
-         K1 = K-1
-         SUM = SUM + RPAR(IPH+K1) * RPAR(IPWDOT+K1) * RPAR(IPWT+K1)
- 100  CONTINUE
-      DELTA(1) = ZP(1) + VOLSP *SUM /CPB
-C
-C         SPECIES EQUATIONS
-C
-      DO 200 K = 1, KK
-         K1 = K-1
-         DELTA(K+1) = ZP(K+1) - RPAR(IPWDOT+K1) *RPAR(IPWT+K1) *VOLSP
- 200  CONTINUE
+!       LOUT   = IPAR(10)
+      ! II     = IPAR(11)
+! C
+! C        MODIFY CHEMKIN WORK ARRAY FOR PRE-EXPONENTIAL
+! C
+!       DO 15 I = 1, II
+!         CALL CKRDEX (-I, RPAR(IPRCK), RPAR(IPRD+I-1))
+! 15    CONTINUE
+! C
+! C         CALL CHEMKIN SUBROUTINES
+! C
+!       CALL CKRHOY (P, Z(1), Z(2), IPAR(IPICK), RPAR(IPRCK), RHO)
+!       CALL CKCPBS (Z(1), Z(2), IPAR(IPICK), RPAR(IPRCK), CPB)
+!       CALL CKWYP  (P, Z(1), Z(2), IPAR(IPICK), RPAR(IPRCK),
+!      1             RPAR(IPWDOT))
+!       CALL CKHMS  (Z(1), IPAR(IPICK), RPAR(IPRCK), RPAR(IPH))
+!       IF (RHO .EQ. 0.0) THEN
+!          WRITE (LOUT, '(/1X,A)') 'Stop, zero density in RCONP.'
+!          STOP
+!       ENDIF
+!       VOLSP = 1. / RHO
+! C
+! C         ENERGY EQUATION
+! C
+!       SUM = 0.
+!       DO 100 K = 1, KK
+!          K1 = K-1
+!          SUM = SUM + RPAR(IPH+K1) * RPAR(IPWDOT+K1) * RPAR(IPWT+K1)
+!  100  CONTINUE
+!       DELTA(1) = ZP(1) + VOLSP *SUM /CPB
+! C
+! C         SPECIES EQUATIONS
+! C
+!       DO 200 K = 1, KK
+!          K1 = K-1
+!          DELTA(K+1) = ZP(K+1) - RPAR(IPWDOT+K1) *RPAR(IPWT+K1) *VOLSP
+!  200  CONTINUE
 
 C ---------- ZDPlasKin ----------
 
-      ! Set intial conditions
+      ! get mole fractions
+      call CKYTX (Z(2), IPAR(IPICK), RPAR(IPRCK), X)
+
+      ! convert to ZDPlasKin unit system
+      R  = 8.31446d+0                        ! Gas constant m3*Pa/K/mole
+      AN = 6.02214d+23                       ! Avogadro number 1/mole
+      P_Pa = P*0.1d+0                        ! Pa
+      density_mole   = 1.0d-6*P_Pa/(R*Z(1))  ! mole/cm3
+      density_number = AN*density_mole      ! /cm3
+
+      dens(:) = density_number*X(:)          ! /cm3
+
+      ! Set properties
       reduced_field_Td = 180.d0
-      gas_temperature_K = 296.0d0
+      gas_temperature_K = Z(1)
       call ZDPlasKin_set_conditions(GAS_TEMPERATURE=gas_temperature_K,
      1                              REDUCED_FIELD=reduced_field_Td)
 
-      ! call ZDPlasKin_set_conditions(SPEC_HEAT_RATIO=spec_heat_ratio, GAS_HEATING=gas_heating )
       density_ini_ch4  = 1.6254d+17
       density_ini_o2   = 3.2705d+17
       density_ini_he   = 1.4688d+18
@@ -91,14 +103,17 @@ C ---------- ZDPlasKin ----------
       call ZDPlasKin_set_density(  'He',density_ini_he)
       call ZDPlasKin_set_density(   'e',density_ini_elec)
 
-      ! Calcurate Energy Eq. (Look at line:47)
-      spec_heat_ratio          = 7.d0/5.d0
-      elec_power_elastic_n     = 0.d0
-      gas_heating              = .true.
-C
+      ! get reaction rates of each species [cm-3*s-1]
       call ZDPlasKin_get_rates(SOURCE_TERMS=source_terms_species)
 
-      write(17, *) (source_terms_species(I), I = 1, II)
+      ! check calcrated reaction rates
+      ! write(17, *) (source_terms_species(I), I = 1, KK)
+      ! write(17, *) R, AN, P, (Z(I), I = 1, KK+1)
+      write(17, *) P_Pa, density_mole, density_number, 
+     1             (dens(I), I = 1, KK)
+
+      ! convert to CHEMKIN unit system
+
 
       RETURN
       END
